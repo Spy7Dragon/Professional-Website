cf8b78a506270c1c61d06e1bde23fd97ec4a9aa2
Spy7Dragon <bh7y@virginia.edu>
2015-12-21 19:58:36 -0500

    PHP Count changes
    Took my ip address off of the counter.

--- count.php
+++ count.php
@@ -3,23 +3,32 @@
 $myFile = file('countlog.txt');
 $count = $myFile[0];
 $date = $myFile[1];
+$me = trim($myFile[2]);
+$viewer = $_SERVER['REMOTE_ADDR'];
 $count = $count + 1;
 
-$newDate = date("m-d-Y", time());
 
-if ($date != $newDate)
+if ($me == $viewer)
 {
-	$emailMsg = "The view count is " . $count;
 
-	if (@mail("bhugg002@odu.edu", "Daily View Count", $emailMsg))
-	{
-
-	}
 }
+else
+{
+	$newDate = date("m-d-Y", time());
 
-$write = fopen("countlog.txt", "w");
-fputs($write, $count . "\r\n");
-fputs($write, $newDate);
-fclose($write);
+	if ($date != $newDate)
+	{
+		$emailMsg = "The view count is " . $count;
+		if (@mail("bhugg002@odu.edu", "Daily View Count", $emailMsg))
+		{
+
+		}
+	}
+	$write = fopen("countlog.txt", "w");
+	fputs($write, $count . "\r\n");
+	fputs($write, $newDate . "\r\n");
+	fputs($write, $me);
+	fclose($write);
+}
 
 ?>
