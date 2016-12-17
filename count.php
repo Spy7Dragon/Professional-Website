f8da86a25d4af4f285f2d5e2d28b454e8a0f40af
Branden Huggins <bhugg002@odu.edu>
2016-12-17 18:40:39 -0500

    Updates

--- count.php
+++ count.php
@@ -7,25 +7,27 @@ $me = trim($myFile[2]);
 $viewer = $_SERVER['REMOTE_ADDR'];
 $count = $count + 1;
 
-
 if ($me != $viewer)
 {
 	$newDate = date("m-d-Y", time());
 
-	if ($date != $newDate)
+	if (strtotime($date) != strtotime($newDate))
 	{
 		$emailMsg = "The view count is " . $count . " on " . $newDate . "\r\n" .
 		"The previous view was on " . $date . ".";
-		if (@mail("bhugg002@odu.edu", "Daily View Count", $emailMsg))
-		{
-
-		}
-	}
+    if (@mail("bhugg002@odu.edu", "Daily View Count", $emailMsg))
+		  echo "Email sent successfully.";
+	  }
+	   else
+	  {
+		  echo "Email could not be sent.";
+	  }
 	$write = fopen("countlog.txt", "w");
 	fputs($write, $count . "\r\n");
 	fputs($write, $newDate . "\r\n");
 	fputs($write, $me);
 	fclose($write);
+  }
 }
 
 ?>
